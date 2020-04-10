c28d4e039c78b46fda5957ab8a201960cd0c13e6
Joe Howarth <josephehowarth@gmail.com>
2020-04-10 17:06:48 -0500

    readInput and makeGraph done

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,24 @@ module Main where
 
 import Lib
 
+import Data.Graph.Inductive.Graph (prettify)
+import Control.Arrow ((>>>))
+
 main :: IO ()
-main = someFunc
+main = do 
+    input <- readInput 
+    case input of 
+        Left err -> fail err
+        Right (Lib.InputFormat {cities, edgeInput, agentInput}) -> do
+            let g = makeGraph cities edgeInput
+            putStrLn $ prettify g
+
+
+
+f :: [Cities] -> EdgeInput -> String
+f cities edgeInput = (prettify . makeGraph ) cities edgeInput
+
+    
+
+
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,23 @@
-{-# LANGUAGE PostfixOperators #-}
 module Lib
-    ( someFunc
+    ( readInput
+    , makeGraph
+    , InputFormat (..)
+    , City
+    , MarketInfo
+    , Good
+    , CityLabel
     ) where
 
-import Data.Graph.Inductive.Graph (mkGraph, prettify)
+import Data.Graph.Inductive.Graph (LNode, LEdge, mkGraph, prettify)
 import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Bifunctor
+import Data.Tuple
 import GHC.Generics
 import Data.Aeson
+import Data.Maybe
 import System.IO
 import Data.Map (Map)
+import qualified Data.ByteString.Lazy as B
 import qualified Data.Map as M
 
 data City = City { label :: CityLabel
@@ -24,17 +33,41 @@ data MarketInfo = MarketInfo { demand :: Int
                              , price :: Int 
                              } deriving (Show, Generic)
 instance FromJSON MarketInfo
+
+data InputFormat = InputFormat { cities :: [City]
+                               , edgeInput :: EdgesInput
+                               , agentInput :: AgentInput
+                               } deriving (Show, Generic)
+instance FromJSON InputFormat
+
+type EdgesInput = [(CityLabel, CityLabel)]
+type AgentInput = [(CityLabel, Int)]
                
 newtype Good = Good { unGood :: String }
-    deriving (Eq, Ord, Show, Generic)
-instance FromJSON Good
-instance FromJSONKey Good
+    deriving (Eq, Ord, Show, FromJSON, FromJSONKey)
 
 newtype CityLabel = CityLabel { unCityLabel :: String }
-    deriving (Show, Generic)
-instance FromJSON CityLabel 
+    deriving (Eq, Ord, Show, FromJSON)
 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+readInput :: IO (Either String InputFormat)
+readInput = B.readFile "input.json" >>= pure . eitherDecode 
+    
+-- type Network = Gr City ()
+
+makeGraph :: [City] -> EdgesInput -> Gr City ()
+makeGraph cities edgesInput = mkGraph nodes edges
+    where
+        nodes = zip [0..length cities - 1] cities
+        labelMap = M.fromList $ first label . swap <$> nodes
+        edges = extend () . both ((M.!) labelMap) <$> edgesInput
+
+both :: (a -> b) -> (a,a) -> (b,b)
+both f = bimap f f 
+
+extend :: c -> (a,b) -> (a,b,c)
+extend c (a,b) = (a,b,c)
+
+
+
 
